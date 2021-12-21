383ad4aee4b8e7ac32ab397bc07193c9c49effa4
ebruyassi <93044958+ebruyassi@users.noreply.github.com>
2021-12-22 00:32:07 +0300

    Firebase bağlantısı yapıldı.

--- lib/views/addresses_screen.dart
+++ lib/views/addresses_screen.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:siparisin_kapinda/globals.dart' as globals;
+import 'package:siparisin_kapinda/globals.dart';
+
+import 'package:siparisin_kapinda/service/firestore_service.dart';
+import 'package:siparisin_kapinda/views/add_address.dart';
+import 'package:siparisin_kapinda/views/profile_screen.dart';
 
 class AddressesPage extends StatefulWidget {
   const AddressesPage({Key? key}) : super(key: key);
@@ -9,14 +17,112 @@ class AddressesPage extends StatefulWidget {
 }
 
 class _AddressesPageState extends State<AddressesPage> {
+  FirestoreService service = FirestoreService();
+  List addresses = [];
+
+  @override
+  void initState() {
+    fetchAddresses();
+  }
+
+  //Adres verileri List'e yazılıyor.
+  void fetchAddresses() async {
+    var res = await service.getUserAddresses(globals.loggedUserId);
+    if (res.isEmpty) {
+      print("Veri yok");
+    } else {
+      setState(() {
+        addresses = res;
+      });
+    }
+  }
+
+  //Adres başlığı ve adres bilgileri yazılıyor.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Adreslerim',
         home: Scaffold(
           backgroundColor: Colors.white,
           appBar: createAppBar(context),
-          body: createbody(),
+          body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('addresses')
+                .where('user_id', isEqualTo: loggedUserId)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      DocumentSnapshot doc = snapshot.data!.docs[index];
+                      return Column(children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(doc['title'],
+                                    textAlign: TextAlign.start,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                        color: Colors.black)),
+                              ),
+                            ),
+                            IconButton(
+                                onPressed: () async {
+                                  FirebaseFirestore.instance
+                                      .collection('addresses')
+                                      .doc(doc['documentId'])
+                                      .delete();
+                                },
+                                alignment: Alignment.centerRight,
+                                icon: const Icon(
+                                  FontAwesomeIcons.trash,
+                                  size: 15,
+                                  color: Color(0xFFE57373),
+                                ))
+                          ],
+                        ),
+                        const Divider(
+                          height: 15,
+                          thickness: 2,
+                          indent: 0,
+                          endIndent: 0,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              const Text(
+                                "Adres: ",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Expanded(
+                                child: Text(
+                                  doc['fulladdress'],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Divider(
+                          height: 15,
+                          thickness: 2,
+                          indent: 0,
+                          endIndent: 0,
+                        ),
+                      ]);
+                    });
+              } else {
+                return const Text("bggnb ");
+              }
+            },
+          ),
           floatingActionButton: FloatingActionButton(
             child: const Icon(
               FontAwesomeIcons.plus,
@@ -24,59 +130,15 @@ class _AddressesPageState extends State<AddressesPage> {
               color: Color(0xFFE57373),
             ),
             backgroundColor: Colors.white,
-            onPressed: () => {},
+            onPressed: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddAddress()),
+              )
+            },
           ),
         ));
   }
-}
-
-//Adresler yazılıyor.
-Widget createbody() {
-  return Container(
-    color: const Color(0xFFFFFFFF),
-    child: ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        const Text(
-          'Ev', //firebase'den çekilecek
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        const Divider(
-          height: 15,
-          thickness: 2,
-          indent: 0,
-          endIndent: 0,
-        ),
-        SizedBox(
-          height: 50,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: const [
-              Text(
-                'Adres:',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(left: 5.0),
-                  child: Text(
-                    'Üniversite, İstanbul Ünv. Cerrahpaşa Avcılar Kampüsü No:2, 34320 Avcılar/İstanbul',
-                  ), //firebase'den çekilecek
-                ),
-              ),
-            ],
-          ),
-        ),
-        const Divider(
-          height: 15,
-          thickness: 2,
-          indent: 0,
-          endIndent: 0,
-        ),
-      ],
-    ),
-  );
 }
 
 //AppBar oluşturuluyor.
