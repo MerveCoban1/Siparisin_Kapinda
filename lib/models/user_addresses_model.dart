574e100f35b1d722773c766f5db4e752c972a639
ebruyassi <93044958+ebruyassi@users.noreply.github.com>
2021-12-22 00:36:15 +0300

    Create user_addresses_model.dart

---
+++ lib/models/user_addresses_model.dart
@@ -0,0 +1,13 @@
+class Addresses {
+  var id;
+  var title;
+  var fulladdress;
+  var documentId;
+
+  Addresses(
+    this.id,
+    this.title,
+    this.fulladdress,
+    this.documentId,
+  );
+}
